eead3691a88ac986d9a349dbaada259a082547fe
Kyle Kirby <k89kirby@gmail.com>
2025-10-25 11:19:08 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (97.86%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        hmap = {
+            ')' : '(',
+            '}' : '{',
+            ']' : '[',
+        }
+
+        for char in s:
+            if char in hmap:
+                if stack and stack[-1] == hmap[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+
+        return True if not stack else False
